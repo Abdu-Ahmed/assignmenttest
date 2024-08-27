322f828646af4e5178b3f2b1bfcb08052baafd93
Abdu-Ahmed <159693431+Abdu-Ahmed@users.noreply.github.com>
2024-08-27 13:03:53 +0300

    Updated Validator class

--- app/core/Validator.php
+++ app/core/Validator.php
@@ -11,33 +11,22 @@ class Validator {
     public const PRODUCT_SELECT = "Please select a product!";
     
     public function validateString(string $value, int $minLength = 1, int $maxLength = PHP_INT_MAX): bool {
-
         $trimmedValue = trim($value);
-        
         $length = mb_strlen($trimmedValue);
-        
-        if ($length < $minLength || $length > $maxLength) {
-            return false;
-        }
-        
-        return preg_match('/^[a-zA-Z0-9]+$/', $trimmedValue) === 1;
+        return $length >= $minLength && $length <= $maxLength;
     }
-
     public function validatePrice(string $value): bool {
-
         $value = trim($value);
-
         $value = str_replace([',', '$', '€', '£'], '', $value);
-
         return filter_var($value, FILTER_VALIDATE_FLOAT) !== false && preg_match('/^\d+(\.\d{1,2})?$/', $value);
     }
     public function validateNotEmpty(string $value): bool {
-
         return trim($value) !== '';
     }
 
     public function validateFloat($value): bool {
 
-        return filter_var($value, FILTER_VALIDATE_FLOAT) !== false;
+        $value = str_replace([','], '', $value);
+        return filter_var($value, FILTER_VALIDATE_FLOAT) !== false && preg_match('/^\d+(\.\d{1,2})?$/', $value);
     }
 }
